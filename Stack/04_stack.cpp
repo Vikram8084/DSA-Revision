7c62ab124398077051ce7a92eb83ed169d80c9d9
Vikram Kumar <vikrambr8084@gmail.com>
2025-02-03 23:25:45 +0530

    middle element

---
+++ Stack/04_stack.cpp
@@ -0,0 +1,58 @@
+//find middle element of the stack
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void solve(stack<int> &st,int &pos,int &ans){
+    //base case
+    if(pos ==1){
+        ans =st.top();
+        return;
+
+    }
+    // 1 case ham solve krenge
+    pos--;
+    int temp = st.top();
+    st.pop();
+
+    //recursion
+    solve(st,pos,ans);
+
+    //backtrack
+    st.push(temp);
+
+}
+
+int getMiddleElement(stack<int>&st){
+    int size =st.size();
+    if(st.empty())
+        return -1;
+    else{
+        //stack is not empty
+        // odd
+        int pos =0;
+        if(size & 1){
+            pos =size/2 +1;
+        }
+        else{
+            //evenn
+            pos =size/2;
+        }
+        int ans =-1;
+        solve(st,pos,ans);
+        return ans;
+    }
+}
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    int mid =getMiddleElement(st);
+    cout<<"Middle element: "<<mid<<endl;
+
+}
